a01cc02d480105cc7d9679a648bba5e06f86da3a
Kulakov <lightofdoctor@gmail.com>
2024-01-17 22:17:18 +0200

    create Unit Test

--- test/unitTest.dart
+++ test/unitTest.dart
@@ -1,24 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
 import 'package:todo/domain/bloc/navigator_bloc.dart';
 import 'package:todo/domain/bloc/sign_in/sign_in_bloc.dart';
 import 'package:todo/domain/bloc/sign_in/sign_in_events.dart';
 import 'package:todo/domain/bloc/sign_in/sign_in_state.dart';
 import 'package:todo/domain/usecase/sign_in_use_case.dart';
 
-class MockSignInBloc extends Mock implements SignInBloc {}
-class MockNavigatorBloc extends Mock implements NavigatorBloc {}
-class MockSignInUseCase extends Mock implements SignInUseCase {}
+import 'unitTest.mocks.dart';
+
+
+@GenerateNiceMocks([MockSpec<SignInUseCase>(), MockSpec<NavigatorBloc>()])
+
+
+
 
 void main() {
-  late MockSignInBloc signInBloc;
+
+  late SignInBloc signInBloc;
   late MockSignInUseCase signInUseCase;
   late MockNavigatorBloc navigatorBloc;
 
   setUp(() {
-    signInBloc = MockSignInBloc();
     signInUseCase = MockSignInUseCase();
     navigatorBloc = MockNavigatorBloc();
+    signInBloc  = SignInBloc(navigatorBloc: navigatorBloc, signInUseCase: signInUseCase);
+
   });
 
 
@@ -28,11 +36,22 @@ void main() {
     signInBloc.add(SignInEvent('losos@gmail.com', '1q2w3e4r5t'));
     await Future.delayed(Duration.zero);
 
-    verify(signInBloc.add(SignInEvent('losos@gmail.com', '1q2w3e4r5t'))).called(1);
+    verify(signInUseCase.execute(any, any)).called(1);
 
-    await untilCalled(signInBloc.state);
-    expectLater(signInBloc.state, isA<UserLoadedState>());
   });
+
+  test('Add SignInEvent and expect UserLoadedState', () async {
+    when(signInUseCase.execute('losos@gmail.com', '1q2w3e4r5t')).thenAnswer((_) async => true);
+
+    signInBloc.add(SignInEvent('losos@gmail.com', '1q2w3e4r5t'));
+
+
+    await Future.delayed(Duration.zero);
+
+
+    expect(signInBloc.state, isA<UserLoadedState>());
+  });
+
   tearDown(() {
     signInBloc.close();
     navigatorBloc.close();
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,75 +1,63 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
 import 'package:todo/domain/bloc/navigator_bloc.dart';
+import 'package:todo/domain/bloc/read_data_users/read_data_users_bloc.dart';
 import 'package:todo/domain/bloc/sign_in/sign_in_bloc.dart';
-import 'package:todo/domain/bloc/sign_in/sign_in_events.dart';
+import 'package:todo/domain/usecase/read_questions_use_case.dart';
+
+
 import 'package:todo/domain/usecase/sign_in_use_case.dart';
 import 'package:todo/presentation/sign_in.dart';
 
-class MockSignInBloc extends Mock implements SignInBloc {}
+import 'widget_test.mocks.dart';
 
-class MockNavigatorBloc extends Mock implements NavigatorBloc {}
-class MockSignInUseCase extends Mock implements SignInUseCase {}
 
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('SignIn Widget Tests', () {
-    late MockSignInBloc signInBloc;
-    late MockNavigatorBloc navigatorBloc;
-    late MockSignInUseCase signInUseCase;
 
-    setUp(() {
-      signInBloc = MockSignInBloc();
-      navigatorBloc = MockNavigatorBloc();
-      signInUseCase = MockSignInUseCase();
-    });
 
-    tearDown(() {
-      signInBloc.close();
-      navigatorBloc.close();
-    });
+@GenerateNiceMocks([MockSpec<SignInUseCase>(), MockSpec<NavigatorBloc>(),MockSpec<ReadQuestionUseCase>()])
 
-    testWidgets('Renders SignIn Screen', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: signInBloc),
-              BlocProvider.value(value: navigatorBloc),
-            ],
-            child: SignIn(),
-          ),
+void main(){
+  late MockNavigatorBloc navigatorBloc;
+  late MockSignInUseCase signInUseCase;
+  late QuestionListBloc questionListBloc;
+  late SignInBloc signInBloc;
+   setUp(() {
+     navigatorBloc = MockNavigatorBloc();
+     signInUseCase = MockSignInUseCase();
+
+     signInBloc = SignInBloc(navigatorBloc: navigatorBloc, signInUseCase: signInUseCase);
+     
+   });
+  testWidgets('Renders SignIn Screen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider<SignInBloc>(
+              create: (context) => signInBloc,
+            ),
+            BlocProvider<NavigatorBloc>(
+              create: (context) => navigatorBloc,
+            ),
+          ],
+          child: SignIn(),
         ),
-      );
+      ),
+    );
 
-      expect(find.byKey(Key('EmailTextField')), findsOneWidget);
-      expect(find.byKey(Key('PasswordTextField')), findsOneWidget);
-      expect(find.byKey(Key('SignInButton')), findsOneWidget);
-      expect(find.byKey(Key('SignUpButton')), findsOneWidget);
-    });
+    expect(find.byKey(Key('EmailTextField')), findsOneWidget);
+    expect(find.byKey(Key('PasswordTextField')), findsOneWidget);
+    expect(find.byKey(Key('SignInButton')), findsOneWidget);
+    expect(find.byKey(Key('SignUpButton')), findsOneWidget);
+  });
 
-    testWidgets('Tapping SignIn', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: signInBloc),
-              BlocProvider.value(value: navigatorBloc),
-            ],
-            child: SignIn(),
-          ),
-        ),
-      );
-
-      when(signInUseCase.execute('losos@gmail.com', '1q2w3e4r5t')).thenAnswer((_) async => true);
-
-      await tester.tap(find.byKey(Key('SignInButton')));
-      await tester.pump();
-
-      verify(signInBloc.add(SignInEvent('losos@gmail.com', '1q2w3e4r5t'))).called(1);
-    });
+  tearDown(() {
+    signInBloc.close();
+    navigatorBloc.close();
   });
 }
